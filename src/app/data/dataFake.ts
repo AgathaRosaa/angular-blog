db6efca2a3c2a64fbfc8c09cbe9886c745f4ac89
agatha rosa <agathagomesrosaa@gmail.com>
2024-02-16 16:52:10 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+
+//É como se fosse um banco de dados ficticio, que quando o usuario passar o id na barra de navegação vai puxar uma dessas informações
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia um novo filme do homem de ferro",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=webp"
+  },
+
+  {
+    "id":"2",
+    "title": "Nova série anunciada na Amazon",
+    "description": "blablabla",
+    "photoCover":"https://i1.sndcdn.com/artworks-c14VJT5PM5cxjdYe-efISiw-t500x500.png"
+  }
+]
